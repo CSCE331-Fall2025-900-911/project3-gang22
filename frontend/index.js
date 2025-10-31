d5d342fe77fc3c28dfb09fc816a278c09a98078d
Aleks Payne <alekspayne4909@gmail.com>
2025-10-31 15:17:57 -0500

    fix bad route to pages

--- frontend/index.js
+++ frontend/index.js
@@ -4,11 +4,11 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // serve the static frontend files
-app.use(express.static(path.join(__dirname, '/frontend/html')));
+app.use(express.static(path.join(__dirname, '/html')));
 
 // default route -> serve index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/html/index.html'));
+  res.sendFile(path.join(__dirname, '../html/index.html'));
 });
 
 app.listen(port, () => {
