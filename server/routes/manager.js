06a77d2e773eaf75fed72780e54bd961458a68c8
Aleks Payne <alekspayne4909@gmail.com>
2025-11-04 19:44:47 -0600

    User/Emp REST API

---
+++ server/routes/manager.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { query } = require('../db');
+
+// --- GET ---
+
+// --- POST ---
+
+// --- DELETE ---
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const employeeRoutes = require('./routes/employee');
 // const managerRoutes = require('./routes/manager');
-// const userRoutes = require('./routes/user');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -14,7 +14,7 @@ app.use(express.json()); // parse JSON bodies
 // Mount route files
 app.use('/api/employees', employeeRoutes);
 // app.use('/api/managers', managerRoutes);
-// app.use('/api/users', userRoutes);
+app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
