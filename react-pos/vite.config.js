9a809c23635123c618b32af3a0e45bf566e0a584
camrgraham <cgraham2200@gmail.com>
2025-11-12 22:10:09 -0600

    more fixes

--- react-pos/vite.config.js
+++ react-pos/vite.config.js
@@ -6,6 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: Number(process.env.PORT) || 4000
+    port: Number(process.env.PORT) || 4000,
+    allowedHosts: [
+      'project3-gang22-frontend.onrender.com',  // allow Render host
+      'localhost'
+    ]
   }
 })
