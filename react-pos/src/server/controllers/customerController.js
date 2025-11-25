e85672242a1a360a7f70f9ac5f842445b2021f28
Juicestus <jus@gtsbr.org>
2025-11-25 14:36:59 -0600

    Fix model not found bug on backend

--- react-pos/src/server/controllers/customerController.js
+++ react-pos/src/server/controllers/customerController.js
@@ -1,6 +1,9 @@
 const { query } = require('../db');
 const menuModel = require('../models/menuModel');
 const orderModel = require('../models/orderModel');
+const orderItemModel = require('../models/orderItemModel');
+const transactionModel = require('../models/transactionModel');
+
 require("dotenv").config({path:'../.env'});
 const { TranslationServiceClient } = require("@google-cloud/translate").v3;
 
@@ -99,14 +102,14 @@ module.exports = {
       });
 
       for (let i = 0; i < menu_ids.length; i++) {
-        await orderModel.addOrderItem(order_id, {
+        await orderItemModel.addOrderItem(order_id, {
           menu_id: menu_ids[i],
           quantity: quantities[i],
           total: totals[i],
         });
       }
 
-      await orderModel.addTransaction(order_id, {
+      await transactionModel.addTransaction(order_id, {
         card_number,
         card_expr_m,
         card_expr_y,
